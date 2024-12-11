94aea800e100a1171107429a0b89c42529faf9ad
curso07 <alejandro.cald@gmail.com>
2024-12-11 18:14:20 +0100

    Funcionan refrescar,, grabar con nuevo y con put, falta borrar y Css

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,86 +1,174 @@
+$(document).ready(function () {
 
-    $("#boton_refresca").on("click", function () {
+    let operacion = null;
+    // función para actualizar la tabla de maestro. 
+    function refrescarListado() {
 
         $.get("http://my-json-server.typicode.com/desarrollo-seguro/dato/solicitudes",
             function (result) {
                 let $padre = $('#listado');
 
+                $padre.empty();
+
                 result.forEach(x => {
                     console.log("Processing item:", x);
                     let $linea = $('<tr>');
-                    $linea.append($('<td class="renglon" style=display:none>').text(x.id));                 
+                    $linea.append($('<td class="renglon" style=display:none>').text(x.id));
                     $linea.append($('<td class="renglon">').text(x.nombre));
                     $linea.append($('<td class="renglon">').text(x.apellido));
                     $linea.append($('<button></button>').text("Borrar"));
                     $padre.append($linea);
-                    
-                   
- 
                 });
+
+                console.log("Refrescaso de tabla ejecutado");
 
             }).fail(function () {
 
                 console.log("Error");
             });
 
-    });
+    };
 
-    $('#listado').on("click", "td", function (event) {
-
+    function mostrarOcultarDetalle() {
         let $detalle = $("detalle");
-        let $row = $(this).closest('tr'); 
-        let solId = $row.find('td').eq(0).text();
-        let solNombre = $row.find('td').eq(1).text(); 
-        let solApellido = $row.find('td').eq(1).text(); 
-
-
-        $("#id").val(solId);
-        $("#nombre").val(solNombre);
-        $("#apellido").val(solApellido);
-
         if ($detalle.is(':visible')) {
             $detalle.hide();
         } else {
             $detalle.show();
         }
 
-        $("#boton_graba").on('click', function (event) {
+    }
 
-            envio = { "id": $("#id").val(),
-                      "nombre": $("#nombre").val(), 
-                      "apellido": $("#apellido").val()};
-            if ($(("#nombre").val() != solNombre) && $(("#apellido").val() != solApellido)) {
+    // Esto es lo que permite que la función sea llamada de cualquier otro lado. 
+    window.refrescarListado = refrescarListado;
+    window.mostrarOcultarDetalle = mostrarOcultarDetalle;
 
-                $.post('https://my-json-server.typicode.com/desarrollo-seguro/dato/solicitudes', // Se `puede hacer ppr ajax también, pero este sería mejor. 
-                    JSON.stringify(envio),
-                    function (response) {
-                        console.log('Respuesta: ' + response)
-                        let $padre = $('<respuesta></respuesta')
-                        let $linea = $('<p></p>').text(JSON.stringify(response));
-                        $padre.append($linea);
+    // Cliquea en botón refrescar listado. 
+    $("#boton_refresca").on("click", function () {
+
+        refrescarListado();
+
+        // clickea en un renglón cualquiera donde hay datos del maestro. 
+        $('#listado').on("click", "td", function (event) {
+
+            event.preventDefault()
+
+            let $row = $(this).closest('tr');
+            let solId = $row.find('td').eq(0).text();
+            let solNombre = $row.find('td').eq(1).text();
+            let solApellido = $row.find('td').eq(2).text();
+
+
+            $("#id").val(solId);
+            $("#nombre").val(solNombre);
+            $("#apellido").val(solApellido);
+
+            operacion = 0; // 0 es modificación. 
+
+            mostrarOcultarDetalle();
+        
             
-                        $('body').append($padre);
-                    });
+        });
 
-            }
-           
+    });
+       
+    $('#boton_nuevo').on("click", function (event) {
+        event.preventDefault(); // Prevent form submission
+        operacion = 1 // 1 significa crear registro. 
+        mostrarOcultarDetalle();
+        $("#id").val("");
+        $("#nombre").val("");
+        $("#apellido").val("");
     });
 
-    $('#boton_cancela').on("click", "td", function (event) {
-
+    $('#boton_cancela').on("click", function (event) {
+        event.preventDefault(); // Prevent form submission
         $("#id").val("");
         $("#nombre").val("");
         $("#apellido").val("");
 
-        if ($detalle.is(':visible')) {
-            $detalle.hide();
-        } else {
-            $detalle.show();
+        mostrarOcultarDetalle();
+
+    });
+
+    $("#boton_graba").on('click', function (event) {
+        
+        event.preventDefault();
+
+        if (operacion == 0) {
+
+
+            let valId = $("#id").val();
+            let valNombre = $("#nombre").val();
+            let valApellido = $("#apellido").val();
+            
+            let envio = {
+                "id": valId,
+                "nombre": valNombre,
+                "apellido": valApellido
+            };
+
+            console.log("Envio Object:", envio);
+
+            $.ajax({
+                url: 'https://my-json-server.typicode.com/desarrollo-seguro/dato/solicitudes/' + envio.id,
+                data: JSON.stringify(envio),
+                method: "PUT",
+                contentType: "application/json",
+                success: function (result) {
+                    console.log('Respuesta: ' + result);
+                    let $padre = $('logUsuario');
+                    let $linea = $('<p></p>').text("Modificación con PUT: "  + JSON.stringify(result));
+                    $padre.append($linea);
+
+                    console.log("Actualización existosa")
+
+                    mostrarOcultarDetalle();
+
+                },
+                error: function (error) {
+                    console.log('Error: ' + error);
+                }
+            });
+
+        } 
+
+        if (operacion == 1) {
+
+            id = 0
+
+            let envio = {
+                "id": id,
+                "nombre": nombre,
+                "apellido": apellido
+            };
+
+            console.log("Envio Object:", envio);
+
+            $.ajax({
+                url: 'https://my-json-server.typicode.com/desarrollo-seguro/dato/solicitudes/',
+                data: JSON.stringify(envio),
+                method: "POST",
+                contentType: "application/json",
+                success: function (result) {
+                    console.log('Respuesta: ' + result);
+                    let $padre = $('logUsuario');
+                    let $linea = $('<p></p>').text("Registro creado con POST: "  + JSON.stringify(result));
+                    $padre.append($linea);
+
+                    console.log("Creación existosa")
+
+                    mostrarOcultarDetalle();
+
+                },
+                error: function (error) {
+                    console.log('Error: ' + error);
+                }
+            });
         }
+
     });
 
 
-    
-    
-    });
 
+});
